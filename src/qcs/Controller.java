cfb38d20096e44c6e5540da28a9f400a73c7a8c7
Ksenia Burova <kburova@vols.utk.edu>
2017-02-12 23:22:01 -0500

    Simple base with drop down menu

--- src/qcs/Controller.java
+++ src/qcs/Controller.java
@@ -1,4 +1,36 @@
+/* Controller.java
+
+This file is a control part of the MVC model.
+It has all the methods to manipulate model
+
+ */
+
 package qcs;
 
-public class Controller {
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
 }
