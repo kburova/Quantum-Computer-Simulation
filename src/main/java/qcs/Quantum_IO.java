c6c1d5435c8ba3f3c830983942ed40dd6ae9b1aa
Nick Kelley <nickdk.1995@gmail.com>
2017-02-28 11:44:56 -0500

    implimented open, except returns string needs to take it another step convert to json then use gson to convert to list of java objects

--- src/main/java/qcs/Quantum_IO.java
+++ src/main/java/qcs/Quantum_IO.java
@@ -1,7 +1,11 @@
 package qcs;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 import java.io.File;
+import java.util.Scanner;
+
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -24,15 +28,40 @@ public class Quantum_IO {
 
     }
 
-    public void load(final Stage mainStage) {
+    public String file_read(File file) {
+
+        StringBuilder json_builder = new StringBuilder();
+
+        {
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            while (scanner.hasNext()) {
+                json_builder
+                        .append(scanner.next())
+                        .append(System.getProperty("line.separator"));
+            }
+            scanner.close();
+        }
+
+        return json_builder.toString();
+    }
+
+    public String load(final Stage mainStage) {
+        //javaFX to choose file natively
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
+        fileChooser.setTitle("Import Quantum File from json");
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
-                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
-                new FileChooser.ExtensionFilter("All Files", "*.*"));
-        File selectedFile = fileChooser.showOpenDialog(mainStage);
-        if (selectedFile != null) mainStage.display(selectedFile);
+                new FileChooser.ExtensionFilter("Json", "*.json"));
+
+        //reads content of file with filescanner and string builder
+        String json_string = file_read(fileChooser.showOpenDialog(mainStage));
+
+        System.out.print(json_string);
+        return json_string;
     }
 }
